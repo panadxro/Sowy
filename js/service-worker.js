c2be640ccf18a9cc0abc7ad90b9c6f87646d3c96
Lucas Panadero <lucasn.panadero@gmail.com>
2024-05-02 03:46:34 -0300

    02/05

---
+++ js/service-worker.js
@@ -0,0 +1,41 @@
+// service-worker.js
+
+const CACHE = 'marketplace-app-cache';
+const urlsAlCache = [
+  '/',
+  'index.html',
+  'offline.html',
+  'css/styles.css',
+  'js/script.js',
+  'img/logo-192x192.png',
+  'img/logo-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(urlsAlCache))
+      .then(() => console.log('Cache abierto y recursos almacenados en caché'))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(CACHE).then(cache => {
+      return cache.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(networkResponse => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          })
+          .catch(() => {
+            return caches.match('/offline.html');
+          })
+        })
+    })
+  );
+});
